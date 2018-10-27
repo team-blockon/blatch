b050fefa384464a7fde486d40faf81589979f92a
zerone2 <czer01ne@gmail.com>
2018-10-27 17:57:04 +0900

    blatch.js modified

--- blatch-frontend/src/lib/caver/blatch.js
+++ blatch-frontend/src/lib/caver/blatch.js
@@ -1,9 +1,13 @@
 import Caver from 'caver-js'
-
-var blatchABI  = require('/blatch-frontend/src/abi/blatch_abi')
+import blatchABI from 'abi/blatch_abi'
 
 Caver.klay.defaultAccount = '1a12d17ffb3e33a5cc092c909bc2d4fc5fd47df6';
 const blatchAddress = '0x77080191ca6a2d5b113ef53d6ebb297084c4f952';
+
+const blatchContract = new Caver.klay.Contract(
+    blatchABI,
+    blatchAddress
+);
 
 export const getAccountInfo = () => {
     return new Promise(async (resolve, reject) => {
@@ -15,29 +19,30 @@ export const getAccountInfo = () => {
 export const saveVideo = ( video_no, video_data ) => {
     return new Promise((resolve, reject) => {
         try {
-            blatchABI.methods.setVideo(video_no, video_data).send({
-                from: Caver.klay.defaultAccount,
-                to: blatchAddress,
-            },
-                (err, txHash) => {
-                    if(!err){
-                        resolve({ txHash });
-                    } else {
-                        reject({ msg: err });
+                blatchContract.methods.setVideo(video_no, video_data)
+                .send({
+                        from: Caver.klay.defaultAccount,
+                        gas: 200000
+                    }, (err, txHash) => {
+                        if(!err){
+                            resolve({ txHash });
+                        } else {
+                            reject({ msg: err });
+                        }
                     }
-                }
-            );
+                )
+                .once('confirmation', () => {
+                    console.log('video save success')
+                });
         } catch (e) {
-            if (e.message === 'invalid address'){
                 console.log("error : " + e.message);
-            }
         }
     });
 };
 
 export const getVideo = ( video_no ) => {
     return new Promise((resolve, reject) => {
-        blatchABI.getVideo(video_no, (err, res) => {
+        blatchContract.methods.getVideo(video_no, (err, res) => {
             if(!err){
                 resolve(res);
             } else {
